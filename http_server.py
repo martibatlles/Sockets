f536acbeb42213269f2b152d9cf9b705e6bb7fd6
Martí Batlles <martibatllescabre@gmail.com>
2020-12-18 17:23:13 +0100

    Primera refactorització

--- http_server.py
+++ http_server.py
@@ -3,14 +3,11 @@ import sys
 import os.path
 
 HOST = "localhost"
-
 PORT = 5555
 
-RESPOSTA = {200, "HTTP/1.1 200 OK\r\n", 
-            404, }
+RESPOSTA = {200: "HTTP/1.1 200 OK\r\n", 
+            404: "HTTP/1.1 404 Not Found\r\n"}
 
-RESPOSTA_OK = "HTTP/1.1 200 OK\r\n"
-RESPOSTA_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n"
 HEADERS = ("Content-Type: text/html\r\n"
 "Server: Marti_Lu\r\n"
 "\r\n")
@@ -20,55 +17,52 @@ def existeix_fitxer(nom_fitxer = "index.html"):
 
 def contingut_fitxer(nom_fitxer = "index.html"):
     log(f"Obrint fitxer: {nom_fitxer}")
-    if nom_fitxer.endswith(".html"):
-        with open(nom_fitxer, "r") as fitxer:
-            contingut = ""
-            for linia in fitxer:
-                contingut += linia
-            return contingut
-    elif nom_fitxer.endswith(".gif"):
-        pass
+    with open(nom_fitxer, "r") as fitxer:
+        contingut = ""
+        for linia in fitxer:
+            contingut += linia
+        return contingut
 
 def log(message):
     print(message)
 
 class GestorCiclePeticioResposta():
     def __init__(self, connexio):
-        self.socket_connectat = SocketConnectat(connexio)
+        self.socket_request = Socket_Request(connexio)
     
     def cicle(self):
         log("Cicle iniciat!")
-        self.request = self.get_request()
-        if self.request.peticio_rebuda:
+        self.request = self.socket_request.dades
+        if self.request:
             self.response = self.make_response()
-            self.socket_connectat.enviar_resposta(self.response)
+            self.socket_request.enviar_resposta(self.response)
        
 
-    def get_request(self):
-        return self.socket_connectat.rebre_request()
-
     def make_response(self):
-        if self.request.metode == "GET": 
+        if self.socket_request.metode == "GET": 
             return self.make_get_response()
 
     
     def make_get_response(self):
         response = ""
-        if existeix_fitxer(self.request.nom_fitxer):
-            codi = RESPOSTA_OK
-            resposta = contingut_fitxer(self.request.nom_fitxer)
+        if existeix_fitxer(self.socket_request.nom_fitxer):
+            codi = RESPOSTA[200]
+            resposta = contingut_fitxer(self.socket_request.nom_fitxer)
 
         else:
-            codi = RESPOSTA_NOT_FOUND
+            codi = RESPOSTA[404]
             resposta = ""
         return f"{codi}{HEADERS}{resposta}"
 
 
-class Request():
-    def __init__(self, dades_peticio):
-        self.peticio_rebuda = dades_peticio
-        if dades_peticio:
-            log(self.peticio_rebuda)
+class Socket_Request():
+    def __init__(self, connexio):
+        (self.socket, self.adreca) = connexio
+        log(f"Connectat des de {self.adreca[0]} (PORT: {self.adreca[1]})")
+        self.rebre_request()
+
+        if self.dades:
+            log(self.dades)
             self.metode = self.get_metode()
             self.recurs = self.get_recurs()
             self.nom_fitxer = self.get_nom_fitxer()
@@ -80,12 +74,27 @@ class Request():
             log("""
         ---------------------
             """)
+
+    def rebre_request(self):
+        log("Rebent petició...")
+        self.dades = self.socket.recv(2048).decode()
+        if not self.dades: self.socket.close()
+        return self.dades
+
+    def enviar_resposta(self, resposta):
+        log("Enviant resposta...")
+        log("""
+        ---------------------
+            """)
+        self.socket.sendall(resposta.encode())
+        self.socket.close()
+
     
     def get_metode(self):
-        return self.peticio_rebuda.split("\n")[0].split(" ")[0]
+        return self.dades.split("\n")[0].split(" ")[0]
 
     def get_recurs(self):
-        return self.peticio_rebuda.split("\n")[0].split(" ")[1]
+        return self.dades.split("\n")[0].split(" ")[1]
     
     def get_nom_fitxer(self):
         if len(self.recurs) > 1:
@@ -93,31 +102,6 @@ class Request():
         else:
             return "index.html"
 
-    @property
-    def check_request(self):
-        return True
-
-
-
-class SocketConnectat():
-    def __init__(self, connexio):
-        (self.socket, self.adreca) = connexio
-        log(f"Connectat des de {self.adreca[0]} (PORT: {self.adreca[1]})")
-
-    def rebre_request(self):
-        log("Rebent petició...")
-        dades = self.socket.recv(2048).decode()
-        if not dades: self.socket.close()
-        peticio = Request(dades)
-        return peticio
-
-    def enviar_resposta(self, dades):
-        log("Enviant resposta...")
-        log("""
-        ---------------------
-            """)
-        self.socket.sendall(dades.encode())
-        self.socket.close()
 
 class Servidor():
     def __init__(self, host = HOST, port = PORT):
@@ -133,7 +117,6 @@ class Servidor():
             while True:
                 self.gestor_cicle_peticio_resposta = GestorCiclePeticioResposta(s.accept())
                 self.gestor_cicle_peticio_resposta.cicle()
-
 
 
 def main():
@@ -152,5 +135,3 @@ if __name__ == "__main__":
     except Exception as exc:
         print("Error:\n")
         print(exc)
-
-    
